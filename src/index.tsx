4531049512500359dd511079a4260e9b0d97cc39
dani001024 <aj9473@naver.com>
2024-03-11 22:01:41 +0900

    font

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import "react-app-polyfill/stable";
 import { GlobalStyle } from "./components/styles";
+import "./fonts/index.css";
 // 이외에 필요한 스타일 파일이 있다면 import 해주세요.
 
 ReactDOM.render(
