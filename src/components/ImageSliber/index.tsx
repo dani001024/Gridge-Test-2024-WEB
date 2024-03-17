9ef20755b708784e85f6271ec53f18e6b88eebd0
dani001024 <aj9473@naver.com>
2024-03-18 00:46:45 +0900

    ImageSlider component

---
+++ src/components/ImageSliber/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Wrapper, ImageContainer,Image,Left,Right,PageIndicator,PageDot} from './styles';
+
+interface Props {
+  images: string[];
+}
+
+
+import { FaChevronCircleLeft,FaChevronCircleRight} from "react-icons/fa";
+
+const ImageSlider: React.FC<Props> = ({ images }) => {
+    const [selected, setSelected] = useState<number>(1);
+    const length: number = images.length;
+  
+    const next = (): void => {
+      setSelected(selected === length ? 1 : selected + 1);
+      console.log(selected);
+    };
+  
+    const pre = (): void => {
+      setSelected(selected === 1 ? length : selected - 1);
+      console.log(selected);
+    };
+    const handleDotClick = (index: number): void => {
+        setSelected(index + 1);
+      };
+  
+    return (<>
+      <Wrapper>
+        <Left onClick={pre} className={selected === 1 ? 'hidden' : ''}>
+          <FaChevronCircleLeft />
+        </Left>
+        <ImageContainer style={{ transform: `translate(-${(selected - 1) * 520}px, 0px)` }}>
+          {images.map((image, index) => (
+            <Image key={index} style={{ backgroundImage: `url(${image})` }} />
+          ))}
+        </ImageContainer>
+        <Right onClick={next} className={selected === length ? 'hidden' : ''}>
+          <FaChevronCircleRight />
+        </Right>
+      </Wrapper>
+      <PageIndicator>
+        {images.map((_, index) => (
+          <PageDot key={index} isSelected={index + 1 === selected}  onClick={() => handleDotClick(index)}/>
+        ))}
+      </PageIndicator>
+      </>
+    );
+  };
+  export default ImageSlider;
---
+++ src/components/ImageSliber/styles.tsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+export const Wrapper = styled.div`
+  position: relative;
+  width: 520px;
+  height: 520px;
+  overflow: hidden;
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  transition: 0.6s;
+  flex-shrink: 0;
+`;
+
+export const Image = styled.div`
+  height: 520px;
+  width: 520px;
+  background-size: cover;
+  background-position: center center;
+  flex-shrink: 0;
+`;
+
+export const Left = styled.span`
+  position: absolute;
+  left: 5px;
+  top: 50%;
+  cursor: pointer;
+  z-index: 1;
+  svg {
+    width: 30px;
+    height: 30px;
+    color: rgba(0, 0, 0, 0.4);
+  }
+  &.hidden {
+    display: none;
+  }
+`;
+
+export const Right = styled.span`
+  position: absolute;
+  right: 5px;
+  top: 50%;
+  cursor: pointer;
+  svg {
+    width: 30px;
+    height: 30px;
+    color: rgba(0, 0, 0, 0.4);
+  }
+  &.hidden {
+    display: none;
+  }
+`;
+
+export const PageIndicator = styled.div`
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  bottom: -20px;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+export const PageDot = styled.div<{ isSelected: boolean }>`
+  width: 5px;
+  height: 5px;
+  border-radius: 50%;
+  margin: 0 5px;
+  background-color: ${({ isSelected }) => (isSelected ? 'rgba(47, 128, 237, 1)' : 'rgba(224, 224, 224, 1)')};
+`;
