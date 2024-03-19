4481a450aa5004cea70f0a58dd4d0817d08be78e
dani001024 <aj9473@naver.com>
2024-03-20 03:40:54 +0900

    API 연결

--- src/components/Subscribe/index.tsx
+++ src/components/Subscribe/index.tsx
@@ -5,8 +5,12 @@ import Payment from "../Payment";
 import { useRecoilValue  } from "recoil";
 import { subscribeState } from "../../recoil/subscribe";
 import Modal from "../Modal";
+import { useLocation } from "react-router-dom";
 
 const Subscribe = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const loginId = searchParams.get('loginId');
   // const resetExampleState = useResetRecoilState(subscribeState);
   const modalBtn = () =>{
     setModal(prev => !prev);
@@ -21,24 +25,26 @@ const isSubscribe = useRecoilValue(subscribeState).subscribeState;
 
   }
   return (
-<Wrapper>
-  <Img src={logo}></Img>
-  {isSubscribe ? (
-    <>
-    <P>서비스를 구독해주셔서 감사합니다.</P>
-    <H4>서비스 구독중</H4>
-    <StyledButton onClick={onClickSubscribeBtn}>구독 해지하기</StyledButton>
-    { modal&&<Modal onClickClose={modalBtn}></Modal>}
-  </>
-  ) : (
+    <Wrapper>
+      <Img src={logo}></Img>
+      {isSubscribe ? (
         <>
-        <P>서비스를 구독하고, 더 다양한 소식을 받아보세요</P>
-        <H4>월 9,900원으로 구독하기</H4>
-        <Payment />
-      </>
-  )}
-  <h5>이용 약관 확인하기</h5>
-</Wrapper>
+          <P>서비스를 구독해주셔서 감사합니다.</P>
+          <H4>서비스 구독중</H4>
+          <StyledButton onClick={onClickSubscribeBtn}>
+            구독 해지하기
+          </StyledButton>
+          {modal && <Modal onClickClose={modalBtn}></Modal>}
+        </>
+      ) : (
+        <>
+          <P>서비스를 구독하고, 더 다양한 소식을 받아보세요</P>
+          <H4>월 9,900원으로 구독하기</H4>
+          {loginId&&<Payment loginId={loginId} />}
+        </>
+      )}
+      <h5>이용 약관 확인하기</h5>
+    </Wrapper>
   );
 };
 export default Subscribe;
--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from "react";
 import { ProfileBox,Wrapper, H4,Container,Icon} from "./styles";
 import { userProfile } from "../../apis/User";
+import { useLocation } from "react-router-dom";
 
 interface UserProfile {
     id: number;
@@ -9,17 +10,19 @@ interface UserProfile {
     followerCount: number;
     followingCount: number;
     feedCount: number;
-    // imageURL: string; // 프로필 이미지 URL
-    // bio: string; // 프로필 설명
-    // additionalInfo: string; // 기타 추가 정보
   }
 const Profile = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const loginId = searchParams.get('loginId');
     const [profile, setProfile] = useState<UserProfile | null>(null);
     useEffect(() => {
-        console.log('Component mounted');
+      console.log(loginId);
+      
+      if (loginId) { // loginId가 정의되어 있는 경우에만 요청 보냄
         const fetchData = async () => {
           try {
-            const response = await userProfile('melody123');
+            const response = await userProfile(loginId);
             console.log('성공', response.result);
             setProfile(response.result);
           } catch (error) {
@@ -29,7 +32,8 @@ const Profile = () => {
         };
     
         fetchData();
-      }, []);
+      }
+    }, [loginId]); 
       if (!profile) {
         return <div>Loading...</div>;
       }
@@ -42,7 +46,7 @@ const Profile = () => {
           <h1>{profile.loginId}</h1>
           <button>프로필 편집</button>
         </Wrapper>
-        <H4>게시물 {profile.feedCount}&nbsp;팔로워 {profile.followerCount}&nbsp;팔로잉 {profile.followingCount}</H4>
+        <H4>게시물 {profile.feedCount}&nbsp;팔로워 {profile.followingCount}&nbsp;팔로잉 {profile.followerCount}</H4>
         {/* <H4>{profile.bio}</H4>
         <StyledPre>{profile.additionalInfo}</StyledPre> */}
       </ProfileBox>
